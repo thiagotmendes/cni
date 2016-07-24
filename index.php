8cc2bc819ae23195ef3de4adaa90c42ef16a9c58
Thiago Mendes <thiagotmendes@outlook.com>
2016-07-24 15:47:35 -0300

    site CNI

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> CNI </title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <header>
+      <section class="barra-superior">
+        <div class="container">
+          <div class="pull-left">
+
+          </div>
+          <div class="pull-right links-superior">
+            <a href="#"> <img src="images/i1.png" alt="" /> Responsabilidade Ambiental</a>
+            <a href="#"> <img src="images/i2.png" alt="" /> Cambio </a>
+            <a href="#"> <img src="images/i3.png" alt="" /> Pesos,Medidas e convenções</a>
+            <div class="clearfix"></div>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </section>
+      <nav class="navbar navbar-cni" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"> <img src="images/logo.png" alt="" /> </a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#"> link </a></li>
+              <li><a href="#"> link </a></li>
+              <li><a href="#"> link </a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+    </header>
+
+    <section>
+      <img src="images/banner-inteiro.jpg" alt="" class="img-responsive" />
+    </section>
+
+    <section class="box-apresenta">
+      <div class="container">
+        <strong> Realizamos o seu projeto </strong> de exportação, <br>
+        Garantindo sucesso no mercado internacional e nacional
+      </div>
+    </section>
+
+    <section class="sevicos inner">
+      <div class="container">
+        <h3 class="titulo-home"> Serviços </h3>
+        <p>
+          Gestão de negóios internacionais.
+          oferecemos serviços diferenciados no seguimento de fretes internacionais, com informações claras,
+          objetivas e confiaveis
+        </p>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="ret-servicos auto">
+              <a href="#"></a>
+            </div>
+            <h3 class="titulo-home">
+              Setor automobilistico
+            </h3>
+          </div>
+          <div class="col-md-3">
+            <div class="ret-servicos setconst">
+              <a href="#"></a>
+            </div>
+            <h3 class="titulo-home">
+              Setor de construção
+            </h3>
+          </div>
+          <div class="col-md-3">
+            <div class="ret-servicos setener">
+              <a href="#"></a>
+            </div>
+            <h3 class="titulo-home">
+              Setor de energia
+            </h3>
+          </div>
+          <div class="col-md-3">
+            <div class="ret-servicos agro">
+              <a href="#"></a>
+            </div>
+            <h3 class="titulo-home">
+              Agronegocio
+            </h3>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="noticia-contato inner">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+            <h4> Ultimas Notícias </h4>
+          </div>
+          <div class="col-md-5">
+            <h4 class="titulo-home"> Entre em contato </h4>
+            <div class="box-formcontato">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="representacao inner">
+      <div class="container">
+        <h3 class="titulo-home"> Representações </h3>
+
+      </div>
+    </section>
+
+    <section  class="mapa inner-map">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="images/icon1.png" alt="" />
+            <p>
+              AV. barão Homem de Melo, 4500 <br>
+              Sala 501 CEP: 30450-250 <br>
+              Belo Horizonte
+            </p>
+          </div>
+          <div class="col-md-4">
+            <img src="images/icon2.png" alt="" />
+            <p>
+              Tel: (31) 2526-6989 <br>
+              Fax: (31) 3024-6148
+            </p>
+          </div>
+          <div class="col-md-4">
+            <img src="images/icon3.png" alt="" />
+            <p>
+              contato@cniconsultoria.com.br
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <div class="container">
+         Copyright &copy; 2016 - CNI Consultoria e Negócios Internacional
+         <div class="pull-right">
+           <a href="#" class="ass"> 4media </a>
+         </div>
+      </div>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-1.12.4.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
